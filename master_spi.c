cbc481df43061f92727fdde45d34a7df90c0c96c
Mati-E <matiasdiazbenini@gmail.com>
2026-03-27 21:15:50 -0300

    implementacion etapa 1 MASTER

--- master_spi.c
+++ master_spi.c
@@ -8,6 +8,7 @@
 #define PIN_CS   17
 #define PIN_SCK  18
 #define PIN_MOSI 19
+#define LED_PIN  PICO_DEFAULT_LED_PIN
 
 static inline void cs_select() {
     gpio_put(PIN_CS, 0);
@@ -19,12 +20,16 @@ static inline void cs_deselect() {
 
 int main() {
     stdio_init_all();
-    sleep_ms(2000);
 
-    printf("MASTER - Etapa 1 Punto 1\n");
-    printf("Enviando un byte por SPI cada 1 segundo\n");
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    spi_init(SPI_PORT, 100 * 1000); // 100 kHz
+    sleep_ms(3000); // da tiempo a que aparezca el USB
+
+    printf("MASTER - Etapa 1 Punto 1\r\n");
+    printf("Enviando un byte por SPI cada 1 segundo\r\n");
+
+    spi_init(SPI_PORT, 10000); // 10 kHz, bien lento para probar
     spi_set_format(SPI_PORT, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
 
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
@@ -35,22 +40,25 @@ int main() {
     gpio_set_dir(PIN_CS, GPIO_OUT);
     cs_deselect();
 
-    uint8_t dato = 0x41; // arranca en 'A'
+    uint8_t dato = 'A';
 
     while (true) {
+        gpio_put(LED_PIN, 1);
+
         cs_select();
-        sleep_us(50);
+        sleep_us(100);
         spi_write_blocking(SPI_PORT, &dato, 1);
-        sleep_us(50);
+        sleep_us(100);
         cs_deselect();
 
-        printf("Enviado: 0x%02X (%c)\n", dato, dato);
+        printf("MASTER envio: 0x%02X (%c)\r\n", dato, dato);
 
         dato++;
-        if (dato > 0x5A) { // después de 'Z' vuelve a 'A'
-            dato = 0x41;
-        }
+        if (dato > 'Z') dato = 'A';
 
-        sleep_ms(1000);
+        sleep_ms(200);
+
+        gpio_put(LED_PIN, 0);
+        sleep_ms(800);
     }
 }
