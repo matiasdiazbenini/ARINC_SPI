d74489e94d5b11411342f7a11f2dfe9cf4d257e6
Mati-E <matiasdiazbenini@gmail.com>
2026-03-27 21:16:18 -0300

    implementacion etapa 1 SLAVE

--- slave_spi.c
+++ slave_spi.c
@@ -9,15 +9,20 @@
 #define PIN_CS   17
 #define PIN_SCK  18
 #define PIN_MOSI 19
+#define LED_PIN  PICO_DEFAULT_LED_PIN
 
 int main() {
     stdio_init_all();
-    sleep_ms(2000);
 
-    printf("SLAVE - Etapa 1 Punto 1\n");
-    printf("Esperando bytes por SPI...\n");
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    spi_init(SPI_PORT, 100 * 1000);
+    sleep_ms(3000); // da tiempo a que aparezca el USB
+
+    printf("SLAVE - Etapa 1 Punto 1\r\n");
+    printf("Esperando bytes por SPI...\r\n");
+
+    spi_init(SPI_PORT, 10000);
     spi_set_slave(SPI_PORT, true);
     spi_set_format(SPI_PORT, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
 
@@ -26,25 +31,27 @@ int main() {
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
 
-    uint8_t dato;
+    uint8_t dato = 0;
 
     while (true) {
-        while (gpio_get(PIN_CS) == 1) {
-            tight_loop_contents();
+        // señal de vida aunque no llegue nada
+        gpio_put(LED_PIN, 1);
+        sleep_ms(50);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(50);
+
+        if (gpio_get(PIN_CS) == 0) {
+            spi_read_blocking(SPI_PORT, 0x00, &dato, 1);
+
+            while (gpio_get(PIN_CS) == 0) {
+                tight_loop_contents();
+            }
+
+            printf("SLAVE recibio: 0x%02X", dato);
+            if (dato >= 32 && dato <= 126) {
+                printf(" (%c)", dato);
+            }
+            printf("\r\n");
         }
-
-        spi_read_blocking(SPI_PORT, 0x00, &dato, 1);
-
-        while (gpio_get(PIN_CS) == 0) {
-            tight_loop_contents();
-        }
-
-        printf("Recibido: 0x%02X", dato);
-
-        if (dato >= 32 && dato <= 126) {
-            printf(" (%c)", dato);
-        }
-
-        printf("\n");
     }
 }
