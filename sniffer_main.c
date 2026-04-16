a015a401114ac63c471fd79f317ac60cccd42567
Agustin <agustinvillanueva18@gmail.com>
2026-04-16 08:49:41 -0300

    Cambio

--- sniffer_main.c
+++ sniffer_main.c
@@ -11,12 +11,32 @@
 
 #define SNIFFER_SYNC_TIMEOUT_US 30000u
 
+typedef enum {
+    SEQ_EXPECT_CMD = 0,
+    SEQ_EXPECT_STS = 1,
+    SEQ_EXPECT_DATA = 2
+} sniffer_seq_state_t;
+
+static const char *type_from_state(sniffer_seq_state_t state) {
+    switch (state) {
+        case SEQ_EXPECT_CMD:
+            return "CMD";
+        case SEQ_EXPECT_STS:
+            return "STS";
+        case SEQ_EXPECT_DATA:
+            return "DATA";
+        default:
+            return "UNKNOWN";
+    }
+}
+
 int main(void) {
     stdio_init_all();
     sleep_ms(1200);
 
     bus_init();
     bus_set_rx_mode();
+    sniffer_seq_state_t seq_state = SEQ_EXPECT_CMD;
 
     while (true) {
         uint16_t word = 0;
@@ -29,9 +49,17 @@ int main(void) {
         }
 
         if (parity_ok) {
-            printf("RX_OK|WORD=0x%04X|PARITY=OK\r\n", word);
+            const char *type = type_from_state(seq_state);
+            printf("RX_OK|TYPE=%s|WORD=0x%04X|PARITY=OK\r\n", type, word);
+
+            if (seq_state == SEQ_EXPECT_DATA) {
+                seq_state = SEQ_EXPECT_CMD;
+            } else {
+                seq_state = (sniffer_seq_state_t)(seq_state + 1);
+            }
         } else {
-            printf("RX_ERR|WORD=0x%04X|PARITY=ERR\r\n", word);
+            printf("RX_ERR|TYPE=UNKNOWN|WORD=0x%04X|PARITY=ERR\r\n", word);
+            seq_state = SEQ_EXPECT_CMD;
         }
 
         fflush(stdout);
