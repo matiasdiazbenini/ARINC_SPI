11f0801315e6429bc28e4082c5fc559f18f27df2
Agustin <agustinvillanueva18@gmail.com>
2026-04-17 14:49:55 -0300

    Cambios

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #define RT_RX_TIMEOUT_US            200000u
 #define RT_TIMEOUT_REPORT_EVERY     20u
 #define RT_RESPONSE_DELAY_US        500u
-#define RT_INTERWORD_GAP_US         7000u
+#define RT_INTERWORD_GAP_US         1000u
 #define RT_POST_TR0_GUARD_US        5000u
 
 static bool rt_is_supported_command(const mil1553_command_word_t *cmd) {
