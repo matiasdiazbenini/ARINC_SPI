f98def43dd8b3494551cd52d3066649159dfb102
Agustin <agustinvillanueva18@gmail.com>
2026-04-26 14:27:54 -0300

    Estable

--- master_spi.c
+++ master_spi.c
@@ -7,7 +7,7 @@
 
 #define TEST_RT_ADDRESS     3u
 #define TEST_SUBADDRESS     1u
-#define TEST_WORD_COUNT     3u
+#define TEST_WORD_COUNT     3u 
 #define TEST_TR             true
 
 int main(void) {
