b19a896335928aa777abb074e87cbca2d4d7f6c7
Agustin <agustinvillanueva18@gmail.com>
2026-05-02 00:24:37 -0300

    PIO RX

--- master_spi.c
+++ master_spi.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-
 #include "pico/stdlib.h"
 #include "bus/bus.h"
 
-int main(void){
+int main(void) {
     stdio_init_all();
     sleep_ms(1200);
 
     bus_init();
     bus_set_tx_mode();
 
-    while (true){
+    while (true) {
         bus_idle();
-        sleep_ms(1000);
+        sleep_ms(500);
 
-        printf("TX_BYTE_F0_TEST\n");
-        
+        printf("TX_BYTE=0xF0\n");
         bus_set_tx_mode();
         bus_send_byte(0xF0);
 
         bus_idle();
-        sleep_ms(2000);
+        sleep_ms(500);
     }
 }
