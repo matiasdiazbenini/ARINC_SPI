f6f6e7392af201f6face3448934120a61c98a2b5
Agustin <agustinvillanueva18@gmail.com>
2026-05-05 04:06:34 -0300

    Intento de halfduplex

--- main.c
+++ main.c
@@ -44,10 +44,12 @@ int main(void) {
              * respondemos Status.
              */
             if (rt == MY_RT_ADDR && tr == BUS_1553_TR_BC_TO_RT) {
-                sleep_us(3000);      // pequeña guarda antes de responder
+                sleep_ms(80);      // pequeña guarda antes de responder
                 bus_set_tx_mode();
 
                 bus_send_status_word(MY_RT_ADDR, false);
+
+                sleep_us(20 * BIT_PERIOD_US);
 
                 bus_idle();
                 bus_set_rx_mode();
