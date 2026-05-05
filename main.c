7ef6dbf6124e58317dcc402cc1822e652acf4c7e
Agustin <agustinvillanueva18@gmail.com>
2026-05-05 00:30:36 -0300

    Mejora en cmd

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include "pico/stdlib.h"
 #include "bus/bus.h"
 
+#define MAX_DATA_WORDS 8u
+
 int main(void) {
     stdio_init_all();
     sleep_ms(1200);
@@ -15,16 +17,24 @@ int main(void) {
 
     while (true) {
         uint16_t cmd = 0;
-        uint16_t data[3] = {0};
+        uint16_t data[MAX_DATA_WORDS] = {0};
 
+        /*
+         * Por ahora seguimos leyendo 3 porque el paquete transmitido tiene 3.
+         * En el paso siguiente hacemos lectura automática real.
+         */
         if (bus_read_packet_checked_pio(&cmd, data, 3)) {
-            printf("PACKET_OK|CMD=0x%04X|D0=0x%04X|D1=0x%04X|D2=0x%04X\n",
-                   cmd,
-                   data[0],
-                   data[1],
-                   data[2]);
+            uint8_t wc = (uint8_t)(cmd & 0x00FFu);
+
+            printf("PACKET_OK|CMD=0x%04X|WC=%u", cmd, wc);
+
+            for (uint8_t i = 0; i < wc && i < MAX_DATA_WORDS; i++) {
+                printf("|D%u=0x%04X", i, data[i]);
+            }
+
+            printf("\n");
         }
 
-        sleep_ms(100);
+        sleep_ms(50);
     }
 }
