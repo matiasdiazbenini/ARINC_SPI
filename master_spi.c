310ebfc91f0ecadbd19f008a39945fe8883b0a06
Agustin <agustinvillanueva18@gmail.com>
2026-05-04 22:02:23 -0300

    Nuevo patron, checksum

--- master_spi.c
+++ master_spi.c
@@ -12,21 +12,20 @@ int main(void) {
         bus_set_tx_mode();
 
         for (int i = 0; i < 100; i++) {
-            // Command/status sync + command word
+            uint16_t cmd = 0x1823;
+            uint16_t d0  = 0xBEEF;
+            uint16_t d1  = 0xCAFE;
+            uint16_t d2  = 0x55AA;
+            uint16_t chk = cmd ^ d0 ^ d1 ^ d2;
+
             bus_send_byte(0xF0);
-            bus_send_byte(0x18);
-            bus_send_byte(0x23);
+            bus_send_word16(cmd);
 
-            // Data sync + data words
             bus_send_byte(0x0F);
-            bus_send_byte(0xA0);
-            bus_send_byte(0x00);
-
-            bus_send_byte(0xA0);
-            bus_send_byte(0x01);
-
-            bus_send_byte(0xA0);
-            bus_send_byte(0x02);
+            bus_send_word16(d0);
+            bus_send_word16(d1);
+            bus_send_word16(d2);
+            bus_send_word16(chk);
         }
     }
 }
