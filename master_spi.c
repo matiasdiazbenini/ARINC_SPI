9d969b54f46f7c800b342d1ff2fbc3454e186021
Agustin <agustinvillanueva18@gmail.com>
2026-08-26 09:42:52 -0300

    Lo ultimo

--- master_spi.c
+++ master_spi.c
@@ -12,47 +12,88 @@
 #define WORD_COUNT  3u
 
 
+static const uint16_t data_test[WORD_COUNT] =
+{
+    0x3333,
+    0x2222,
+    0x1111
+};
+
+
 int main(void)
 {
     stdio_init_all();
 
     sleep_ms(1200);
 
+    /*
+     * Inicializa el transmisor físico PIO.
+     */
     mil1553_tx_init();
 
 
     /*
-     * TR = 1
-     * RT = 3
-     * SUB = 7
-     * WC = 3
+     * Receive Command:
      *
-     * CMD esperado = 0x1CE3
+     * RT  = 3
+     * TR  = 0  -> BC transmite hacia RT
+     * SUB = 7
+     * WC  = 3
+     *
+     * CMD esperado = 0x18E3
      */
-
     const uint16_t cmd =
         BUS_1553_CMD_MAKE(
             RT_ADDR,
-            BUS_1553_TR_RT_TO_BC,
+            BUS_1553_TR_BC_TO_RT,
             SUBADDR,
-            WORD_COUNT);
-
-
-    printf("MIL1553 REAL TX TEST\n");
-    printf("CMD=0x%04X\n", cmd);
-
-
-   const uint16_t data_test = 0xA5A5;
-
-    uint32_t tx_count = 0;
-
-    while (true)
-    {
-        mil1553_tx_send_word(
-            BUS_1553_SYNC_DATA,
-            0xA5A5
+            WORD_COUNT
         );
 
-        sleep_ms(500);
-    }
+
+    printf("MIL-STD-1553 BC - CMD + 3 DATA\n");
+
+    printf(
+        "CMD: RT=%u TR=0 SUB=%u WC=%u -> 0x%04X\n",
+        RT_ADDR,
+        SUBADDR,
+        WORD_COUNT,
+        cmd
+    );
+
+    printf("DATA[0] = 0x%04X\n", data_test[0]);
+    printf("DATA[1] = 0x%04X\n", data_test[1]);
+    printf("DATA[2] = 0x%04X\n\n", data_test[2]);
+
+
+    while (true)
+{
+    mil1553_tx_send_word(
+        BUS_1553_SYNC_CMD_STATUS,
+        cmd
+    );
+
+    sleep_us(5);
+
+    mil1553_tx_send_word(
+        BUS_1553_SYNC_DATA,
+        data_test[0]
+    );
+
+    sleep_us(5);
+
+    mil1553_tx_send_word(
+        BUS_1553_SYNC_DATA,
+        data_test[1]
+    );
+
+    sleep_us(5);
+
+    mil1553_tx_send_word(
+        BUS_1553_SYNC_DATA,
+        data_test[2]
+    );
+
+    sleep_ms(500);
+}
 }
